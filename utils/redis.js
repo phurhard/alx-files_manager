c3aad4a07f6e626c4126e955fef5128f82ca3c3f
phurhard <phurhardeen@gmail.com>
2024-02-13 12:12:21 +0100

    redis

---
+++ utils/redis.js
@@ -0,0 +1,52 @@
+const redis = require ('redis');
+
+
+class RedisClient {
+    constructor(options) {
+        this.client = redis.createClient(options);
+    
+        this.client.on('error', (err) => {
+            console.log(err);
+        });
+
+        this.getAsync = promisify(this.client.get).bind(this.client);
+        this.setAsync = promisify(this.client.set).bind(this.client);
+        this.delAsync = promisify(this.client.del).bind(this.client);
+    }
+
+    isAlive() {
+        return this.client.connected;
+    }
+    this
+
+    async get(key) {
+        try {
+            const value =  await this.getAsync(key);
+            return value;
+        } catch (err) {
+            console.log(err);
+            throw error;
+        }
+    }
+
+    async set(key, value, ttl) {
+        try {
+            await this.setAsync(key, value, 'EX', ttl);
+        } catch (err) {
+            console.log(err);
+            throw error;
+        }
+    }
+
+    async del(key) {
+        try {
+            await this.delAsync(key);
+        } catch (err) {
+            console.log(err);
+            throw error;
+        }
+
+    }
+}
+
+module.exports = RedisClient;
